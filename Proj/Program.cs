dbb067e0a713c961bf3dc27260dc041e722e9391
Sasha <alecsand.unit@gmail.com>
2025-03-21 15:39:47 +0200

    Добавил несколько Console.WriteLine

--- Proj/Program.cs
+++ Proj/Program.cs
@@ -11,3 +11,12 @@ Console.WriteLine("8");
 Console.WriteLine("9");
 Console.WriteLine("10");
 
+Console.WriteLine("Sasha - 1");
+Console.WriteLine("Sasha - 2");
+Console.WriteLine("Sasha - 3");
+Console.WriteLine("Sasha - 4");
+Console.WriteLine("Sasha - 5");
+
+
+
+
